c1274e815ed12e1720e2c247957e95e95a0878b3
Jacek Smolak <jacek.smolak@gmail.com>
2017-09-09 20:40:04 +0200

    Allow passing options to testNamespace
    Some adapters will pass `namespace` not as only parameter in options and for namespace testing passing additional parameters is sometimes required (to not trigger validation of those other parameters).

--- src/validatonTests.js
+++ src/validatonTests.js
@@ -2,14 +2,10 @@ import { expect } from 'chai';
 
 import { invalidCharacters, nonStringValues } from './valueSets';
 
-function createOptions(options) {
-    return Object.assign({}, { namespace: 'namespace' }, options);
-}
-
 export function testKey(action) {
     context('when key is not of string type', () => {
         it('should throw', () => {
-            nonStringValues.forEach(key => {
+            nonStringValues.forEach((key) => {
                 expect(action.bind(null, key)).to.throw('`key` must be a string.');
             });
         });
@@ -17,26 +13,30 @@ export function testKey(action) {
 
     context('when key contains characters other than a-z, A-Z, 0-9, `-` and `_`', () => {
         it('should throw', () => {
-            invalidCharacters.forEach(key => {
+            invalidCharacters.forEach((key) => {
                 expect(action.bind(null, key)).to.throw('`key` can contain only letters, numbers, `_`, `.` or `-`.');
             });
         });
     });
 }
 
-export function testNamespace(action) {
+export function testNamespace(action, options = {}) {
     context('when namespace is not of string type', () => {
         it('should throw', () => {
-            nonStringValues.forEach(namespace => {
-                expect(action.bind(null, createOptions({ namespace }))).to.throw('`namespace` must be a string.');
+            nonStringValues.forEach((namespace) => {
+                const mergedOptions = Object.assign({}, options, { namespace });
+
+                expect(action.bind(null, mergedOptions)).to.throw('`namespace` must be a string.');
             });
         });
     });
 
     context('when namespace contains characters other than a-z, A-Z, 0-9, `-` and `_`', () => {
         it('should throw', () => {
-            invalidCharacters.forEach(namespace => {
-                expect(action.bind(null, createOptions({ namespace }))).to.throw(
+            invalidCharacters.forEach((namespace) => {
+                const mergedOptions = Object.assign({}, options, { namespace });
+
+                expect(action.bind(null, mergedOptions)).to.throw(
                     '`namespace` can contain only letters, numbers, `_` or `-`.'
                 );
             });
