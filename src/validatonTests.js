e6b97f172cc555c088a7b2619a9667d7a3d81f74
Jacek Smolak <jacek.smolak@gmail.com>
2018-06-24 17:20:55 +0200

    Remove invalid characters validation
    Upcomming adapters won't have any other than is-a-strong validation.
    This is because the responsibility for what characters are used
    upon creating a key will rely on the end user.

--- src/validatonTests.js
+++ src/validatonTests.js
@@ -1,20 +1,12 @@
 import { expect } from 'chai';
 
-import { invalidCharacters, nonStringValues } from './valueSets';
+import { nonStringValues } from './valueSets';
 
 export function testKey(action) {
     context('when key is not of string type', () => {
         it('should throw', () => {
             nonStringValues.forEach((key) => {
                 expect(action.bind(null, key)).to.throw('`key` must be a string.');
-            });
-        });
-    });
-
-    context('when key contains characters other than a-z, A-Z, 0-9, `-` and `_`', () => {
-        it('should throw', () => {
-            invalidCharacters.forEach((key) => {
-                expect(action.bind(null, key)).to.throw('`key` can contain only letters, numbers, `_`, `.` or `-`.');
             });
         });
     });
